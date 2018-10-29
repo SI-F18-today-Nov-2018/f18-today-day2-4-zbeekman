66df1dd52188bce4ebbc620a52d8fbdec318fe2e
Damian Rouson <damian@sourceryinstitute.org>
2018-10-29 12:26:04 -0700

    First trivially passing version of Day-1/tests

---
+++ src/Day-1/tests/test-cartesian-grid.f90
@@ -0,0 +1,7 @@
+program main
+  !! Test cartesian grid definition
+ !use cartesian_grid_interface, only : cartesian_grid
+  implicit none
+ !type(cartesian_grid) :: mesh
+  print *,"Test passed."
+end program
