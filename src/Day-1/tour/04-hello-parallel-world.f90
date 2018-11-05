0f4e2681a6952da22546a30634752a9375eb965d
Damian Rouson <damian@sourceryinstitute.org>
2018-11-04 21:45:55 -0800

    Add hello-parallel-world.f90

--- src/Day-1/tour/04-hello-parallel-world.f90
+++ src/Day-1/tour/04-hello-parallel-world.f90
@@ -1,9 +1,17 @@
 program main
   use assertions_interface, only : assert,assertions
-  integer, parameter :: test_reporter=1
-  associate(me=>this_image())
-    if (assertions) call assert(.false.,"tautology")
+  integer, parameter :: test_reporter=1, max_string=100
+  character(len=max_string) greeting[*],message_expected,message_received
+  associate(me=>this_image(),ni=>num_images())
+    write(greeting,"(2(a,i2))") "Hello from image",this_image(),"of",num_images()
     sync all
-    if (me==test_reporter) print *,"Test passed."
+    if (me==test_reporter) then
+      do concurrent(image=1:ni)
+        print *,greeting[image]
+        write(message_expected,"(2(a,i2))") "Hello from image",image,"of",ni
+        if (assertions) call assert(message_expected==greeting[image],"received expected message")
+      end do
+      print *,"Test passed."
+    end if
   end associate
 end program
